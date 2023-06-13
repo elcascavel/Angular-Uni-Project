8d5ad6a6f49c8e4d798285b497d389a16a97d23c
Cascavel <simao.ramaral@gmail.com>
2023-06-13 22:47:01 +0100

    Quick readability updates, BEM fixes etc

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,19 +7,22 @@ import { ARTISTAS } from '../modelos/artista-list';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
-  artistas = ARTISTAS;
-
-  conteudoVisivelMap: { [key: number]: boolean} = {};
-
-  onClick(index: number)
-  {
-    this.conteudoVisivelMap[index] = !this.conteudoVisivelMap[index];
-  }
+  artistas: Artista[] = ARTISTAS;
+  conteudoVisivelMap: { [key: number]: boolean } = {};
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onClick(index: number): void {
+    this.conteudoVisivelMap[index] = !this.conteudoVisivelMap[index];
+  }
+}
+
+interface Artista {
+  name: string;
+  img: string;
+  emoji: string;
+  album: string[];
 }
